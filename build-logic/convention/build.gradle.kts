e694cc269846961cd1727d5601ef47ddaa1e4cb8
Arthur Aguiar <arthur.aguiar@ifood.com.br>
2024-04-17 16:52:51 -0300

    configurando plugin do build-logic

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "br.com.config.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.build.logic.android.gradlePlugin)
+    compileOnly(libs.build.logic.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+
+    plugins {
+
+        register("androidLibrary") {
+            id = "config.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,27 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+        maven {
+            url = uri("https://dl.google.com/dl/android/maven2")
+        }
+    }
+    dependencies {
+        classpath (libs.build.logic.android.gradlePlugin)
+        classpath (libs.build.logic.kotlin.gradlePlugin)
+
+    }
+}
+
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.ksp) apply false
+}
+
+tasks.register("clean",Delete::class){
+    delete(rootProject.layout.buildDirectory)
 }
