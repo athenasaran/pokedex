f6618a401fc6b3e8dc5cad45b223efd10ef73e60
Athena F Sarantopoulos <athenasaran@gmail.com>
2025-04-15 15:36:07 -0300

    Wrap DefaultErrorContent in a Column for improved layout in FavoriteScreen

--- features/src/main/kotlin/com/athena/features/favorite/presentation/view/FavoriteScreen.kt
+++ features/src/main/kotlin/com/athena/features/favorite/presentation/view/FavoriteScreen.kt
@@ -1,16 +1,26 @@
 package com.athena.features.favorite.presentation.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.athena.designsystem.R
 import com.athena.designsystem.components.error.DefaultErrorContent
 
 @Composable
 fun FavoriteScreen(modifier: Modifier = Modifier) {
-    DefaultErrorContent(
-        imageError = R.drawable.img_not_found_favorite,
-        title = "You haven't favorited any Pokémon :( ",
-        modifier = modifier,
-        subTitle = "Click on the heart icon of your favorite Pokémon and they will appear here."
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        DefaultErrorContent(
+            imageError = R.drawable.img_not_found_favorite,
+            title = "You haven't favorited any Pokémon :( ",
+            modifier = modifier,
+            subTitle = "Click on the heart icon of your favorite Pokémon and they will appear here."
+        )
+    }
 }
