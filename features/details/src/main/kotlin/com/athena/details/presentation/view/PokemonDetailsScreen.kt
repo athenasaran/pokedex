011d2f96ad3a5b0ba8887ab77afde55c325aaaec
Athena F Sarantopoulos <athenasaran@gmail.com>
2026-04-22 20:09:27 -0300

    Create animation for image of pokemon on details screen

--- features/details/src/main/kotlin/com/athena/details/presentation/view/PokemonDetailsScreen.kt
+++ features/details/src/main/kotlin/com/athena/details/presentation/view/PokemonDetailsScreen.kt
@@ -7,7 +7,10 @@ import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.EaseOutBounce
 import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -35,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -57,6 +61,7 @@ import com.athena.details.presentation.state.PokemonDetailsState
 import com.athena.details.presentation.viewmodel.PokemonDetailsViewModel
 import com.athena.domain.model.details.PokemonDetails
 import com.athena.domain.model.details.Type
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
@@ -96,6 +101,22 @@ private fun SharedTransitionScope.PokemonDetailsContent(
     val context = LocalContext.current
     var isFavoriteClicked by remember { mutableStateOf(pokemonDetails.isFavorite) }
     val iconFavorite = if (isFavoriteClicked) DesignSystemDrawableRes.ic_favorite_clicked else DesignSystemDrawableRes.ic_favorite
+    var shouldAnimate by remember { mutableStateOf(false) }
+    var isImageClicked by remember { mutableStateOf(false) }
+
+    val bounceScale by animateFloatAsState(
+        targetValue = if (shouldAnimate) 1f else 1.2f,
+        animationSpec = tween(1000, easing = EaseOutBounce)
+    )
+
+    val scale by animateFloatAsState(
+        targetValue = if (isImageClicked) 1.2f else 1f,
+        animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing)
+    )
+
+    LaunchedEffect(Unit) {
+        shouldAnimate = true
+    }
 
     Column(
         modifier = modifier
@@ -120,14 +141,25 @@ private fun SharedTransitionScope.PokemonDetailsContent(
                 contentDescription = null,
                 modifier = Modifier
                     .padding(8.dp)
+                    .graphicsLayer {
+                        scaleX = scale
+                        scaleY = scale
+                    }
                     .clickable {
                         isFavoriteClicked = !isFavoriteClicked
+                        isImageClicked = true
+
                         onIntent(
                             PokemonDetailsIntent.OnFavoriteClick(
                                 pokemonName = pokemonDetails.name,
                                 isFavorite = isFavoriteClicked
                             )
                         )
+
+                        coroutineScope.launch {
+                            delay(300)
+                            isImageClicked = false
+                        }
                     }
             )
             AsyncImage(
@@ -135,7 +167,11 @@ private fun SharedTransitionScope.PokemonDetailsContent(
                 modifier = Modifier
                     .align(Alignment.Center)
                     .padding(vertical = 36.dp)
-                    .size(150.dp),
+                    .size(150.dp)
+                    .graphicsLayer {
+                        scaleX = bounceScale
+                        scaleY = bounceScale
+                    },
                 model = ImageRequest.Builder(context)
                     .data(pokemonDetails.urlImage)
                     .allowHardware(false)
