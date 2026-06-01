a1d5e5f56537fcfd5e227d07d0ff0c867615514e
Athena F Sarantopoulos <athenasaran@gmail.com>
2026-06-01 20:30:38 -0300

    Fix delete swipe on Favorite Screen

--- features/favorite/src/main/kotlin/com/athena/favorite/presentation/view/FavoriteScreen.kt
+++ features/favorite/src/main/kotlin/com/athena/favorite/presentation/view/FavoriteScreen.kt
@@ -78,7 +78,10 @@ private fun FavoriteContent(
     LazyColumn(
         modifier.padding(vertical = 8.dp, horizontal = 6.dp)
     ) {
-        items(favorites) { favorite ->
+        items(
+            items = favorites,
+            key = { it.name }
+        ) { favorite ->
             CardFavorite(
                 modifier.padding(vertical = 8.dp),
                 pokemonName = favorite.name,
